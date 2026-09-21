8196f886a7f38f97ba3cb566be7e9202f85f8602
FidaHussainLarik <fidahussainlarik.bscsf24@iba-suk.edu.pk>
2026-09-21 22:07:57 +0500

    feat: practice class activities of string

---
+++ week_05_javascript/basics.js
@@ -0,0 +1,22 @@
+const course = "Web Eng" 
+
+// Reassignemnt of constant variable throuws errors
+// course = "Other"
+
+console.log("Constant variable: ",course)
+
+
+let count = 0
+count = count + 1
+count = count + 1
+console.log("Count: ",count)
+
+// auto type conversion
+console.log(0 == false)
+
+// strict equality -checks the datatypes are different and return false
+console.log(0 === false)
+
+// Empty array == 0 & false == 0 ---> TRUE
+console.log([] == false)
+
---
+++ week_05_javascript/strings.js
@@ -0,0 +1,41 @@
+/*
+const first_name = "Fida"
+const last_name = "Hussain"
+
+const fullName = `${first_name} ${last_name}`
+
+console.log("First Name  : ",first_name)
+console.log("Second Name : ",last_name)
+console.log("Full   Name : ",fullName)
+
+console.log("Length of fullName: ",fullName.length)
+console.log("Uppercase of fullName: ",fullName.toUpperCase())
+console.log("Includes 'Fida': ",fullName.includes('Gada '))
+
+
+
+console.log("Slice: ",fullName.slice(5,))
+
+// Slpit at space
+list = fullName.split(' ')
+console.log("List: ",list)
+
+
+let num1 = 0.78
+let num2 = 0.
+// fix the nnumber of points after decimal
+let sum = (num1 + num2).toFixed(3)
+console.log("Sum of ", num1, " and ",num2," is: ", sum )
+
+
+let number = "2004"
+number = number 
+console.log("Date of birth in string from: ", number)
+
+let date_of_birth = Number(number)
+console.log("Date of birth: ",date_of_birth+1)
+
+*/
+
+
+
